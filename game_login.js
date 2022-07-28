74d2821269a3e829b050d3d6fb7ea1eabd593be8
aadita1234 <100841895+aadita1234@users.noreply.github.com>
2022-07-28 14:18:52 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function addUser() {
+    user_name = document.getElementById("user_name_input").value;
+    
+  
+      localStorage.setItem("user_name", user_name);
+      
+  
+      window.location = "game_page.html";
+  }
+  
